b1fab917ea86838109c4fcc003bb702aa50274db
jtbuaa <jtbuaa@cc3c3b66-2126-11df-b9f8-bfe9128881a6>
2010-06-23 03:36:57 +0000

    Jingtao: add parse heade

--- trunk/cts/parsects.py
+++ trunk/cts/parsects.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python
 #Filename: parsercts.py
-#Jingtao: 2010-05-20
+#Jingtao: 2010-06-23
 
 import xml.sax
 import csv
 import sys
 
-global_mapping = {}
+target_mapping = {} #to store case info of the test result
 
 class resultHandler(xml.sax.handler.ContentHandler):
-  global global_mapping
+  global target_mapping
 
   def __init__(self, isTarget):
      self.packageName = ""
@@ -19,9 +19,29 @@ class resultHandler(xml.sax.handler.ContentHandler):
 
      self.isTarget = isTarget
      self.mapping = {}
+     self.header = {}
 
   def startElement(self, name, attrs):
-     if name == "TestSuite" or name == "TestCase":
+     if name == "TestResult":
+         self.header["test endtime"] = attrs["endtime"]
+         self.header["test starttime"] = attrs["starttime"]
+         self.header["CTS plan version"] = attrs["version"]
+     elif name == "BuildInfo":
+         self.header["androidPlatformVersion"] = attrs["androidPlatformVersion"]
+         self.header["buildName"] = attrs["buildName"]
+         self.header["build_brand"] = attrs["build_brand"]
+         self.header["build_device"] = attrs["build_device"]
+         self.header["build_fingerprint"] = attrs["build_fingerprint"]
+         self.header["build_model"] = attrs["build_model"]
+         self.header["build_type"] = attrs["build_type"]
+         self.header["imei"] = attrs["imei"]
+         self.header["imsi"] = attrs["imsi"]
+         self.header["locales"] = "\"" + attrs["locales"] + "\""
+         self.header["network"] = attrs["network"]
+     elif name == "Cts":
+         self.header["CTS version"] = attrs["version"]
+
+     elif name == "TestSuite" or name == "TestCase":
 	 if self.packageName == '':
 	      self.packageName = attrs['name']
 	 else:
@@ -43,7 +63,7 @@ class resultHandler(xml.sax.handler.ContentHandler):
         if self.testResult != "pass" and self.testResult != "notExecuted":
           self.mapping[self.packageName + "#" + self.caseName] = [self.testResult, self.errMsg]
       else:
-        if self.packageName + "#" + self.caseName in global_mapping:#record all the result if is base
+        if self.packageName + "#" + self.caseName in target_mapping:#record all the result if is base and the result found in target
           self.mapping[self.packageName + "#" + self.caseName] = [self.testResult, self.errMsg]
       #clear the variables relate to current case
       self.caseName = ""
@@ -59,32 +79,40 @@ class resultHandler(xml.sax.handler.ContentHandler):
 #para input
 def print_useage():
     cmd = sys.argv[0]
-    print "input 3 path parameters:1.the path of CTS testResult.xml file as base. 2.the path of CTS testResult.xml file to compare with the base one."
-    print "                        3.the third paremeter is optional:The path of compare result file"
-    print "2 parameters example:python " + cmd + " dragon-r10000/testResult.xml" + " dragon-r12000/testResult.xml"
-    print "3 parameters example:python " + cmd + " nexus-one/testResult.xml" + " dragon-r12000/testResult.xml" + " compare.csv"
+    print "usage: " + cmd + " base target [output-file] [revert-base-target] [compare-header]"
+    print "example: "
+    print "       " + cmd + " dragon-r10000/testResult.xml" + " dragon-r12000/testResult.xml"
+    print "       " + cmd + " nexus-one/testResult.xml" + " dragon-r12000/testResult.xml" + " compare.csv"
+    print "       " + cmd + " nexus-one/testResult.xml" + " dragon-r12000/testResult.xml" + " compare.csv 0 1"
 
 
 args = sys.argv[1:]
 if (len(args) < 2):
     print_useage() 
 else:
+    baseResult = sys.argv[1]
+    targetResult = sys.argv[2]
+    if (len(args) >= 4) and (sys.argv[4] == "1"):
+        baseResult = sys.argv[2]
+        targetResult = sys.argv[1]
+
     parser = xml.sax.make_parser()
     handler_base = resultHandler(False)
     handler_target = resultHandler(True)
 
     parser.setContentHandler(handler_target)
-    parser.parse(sys.argv[2])
-    global_mapping = handler_target.mapping
+    parser.parse(targetResult)
+    target_mapping = handler_target.mapping
+    target_header = handler_target.header
 
     parser.setContentHandler(handler_base)
-    parser.parse(sys.argv[1])
+    parser.parse(baseResult)
 
     results = []
-    for k in global_mapping:
+    for k in target_mapping:
         package = k.split("#")[0]
         case = k.split("#")[-1]
-        target_value = global_mapping.get(k)
+        target_value = target_mapping.get(k)
         target_result = target_value[0]#fail or timeout
         base_value = handler_base.mapping.get(k)
         compareInfo = ""
@@ -112,9 +140,19 @@ else:
             print result
     results.sort() #sort results by compare info
 
-    if len(args) == 3:
+    if len(args) >= 3:
         csvWriter = csv.writer(file(sys.argv[3], 'w'))
 	csvWriter.writerow(['Test Package', 'Test Cases', 'compare to base version\n' + sys.argv[1], 'CTS Error' + '(' + sys.argv[2] + ')'])
         for i in results:
             result = [i[1], i[2], i[0], i[3].encode("utf-8")] # need to convert err msg to utf-8 otherwise it may report exception
             csvWriter.writerow(result)
+
+    headers = []
+    if len(args) >= 5 and sys.argv[5] == "1":
+        csvWriter.writerow("") 
+        csvWriter.writerow("") 
+        for i in target_header:
+            headers.append([i, target_header[i]])
+        headers.sort()
+        for i in headers:
+            csvWriter.writerow(i) 

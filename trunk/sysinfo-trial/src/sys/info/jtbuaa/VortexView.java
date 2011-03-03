7a34f2c803a9cf24bcbe5dea9336dca0f237e01d
jtbuaa <jtbuaa@cc3c3b66-2126-11df-b9f8-bfe9128881a6>
2011-03-03 04:02:12 +0000

    jingtao: add GL Vender

---
+++ trunk/sysinfo-trial/src/sys/info/jtbuaa/VortexView.java
@@ -0,0 +1,19 @@
+package sys.info.jtbuaa;
+
+import android.content.Context;
+import android.opengl.GLSurfaceView;
+import android.view.MotionEvent;
+
+public class VortexView extends GLSurfaceView {
+    private VortexRenderer _renderer;
+    
+    public VortexView(Context context) {
+        super(context);
+        _renderer = new VortexRenderer();
+        setRenderer(_renderer);
+    }
+    
+	public String getGlVender() {
+        return _renderer.glVender;
+	}
+}

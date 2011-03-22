5abc79a3b3d9119f15a6fb9bbc9f3e78056c59cf
jtbuaa <jtbuaa@cc3c3b66-2126-11df-b9f8-bfe9128881a6>
2011-03-22 08:42:42 +0000

    jingtao: mv ifprint-jni here, add cpufeatures, build by ndk-r5b

---
+++ trunk/sysinfo-trial/jni/dmesg.c
@@ -0,0 +1,44 @@
+/* vi: set sw=4 ts=4: */
+/*
+ *
+ * dmesg - display/control kernel ring buffer.
+ *
+ * Copyright 2006 Rob Landley <rob@landley.net>
+ * Copyright 2006 Bernhard Reutner-Fischer <rep.nop@aon.at>
+ *
+ * Licensed under GPLv2, see file LICENSE in this tarball for details.
+ */
+#include <sys/klog.h>
+#include <stdlib.h>
+#include <jni.h>
+
+char *buf;
+void dmesg()
+{
+	int len, level;
+
+	len = klogctl(10, NULL, 0); /* read ring buffer size */
+	if (len < 16*1024)
+		len = 16*1024;
+	if (len > 16*1024*1024)
+		len = 16*1024*1024;
+
+	buf = malloc(len);
+	len = klogctl(3, buf, len); /* read ring buffer */
+}
+
+jstring Java_sys_info_jtbuaa_Properties_dmesg( JNIEnv* env, jobject thiz )
+{
+    dmesg();
+    jstring mesg = (*env)->NewStringUTF(env, buf);
+    free(buf);
+    return mesg;
+}
+
+jstring Java_system_info_Properties_dmesg( JNIEnv* env, jobject thiz )
+{
+    dmesg();
+    jstring mesg = (*env)->NewStringUTF(env, buf);
+    free(buf);
+    return mesg;
+}

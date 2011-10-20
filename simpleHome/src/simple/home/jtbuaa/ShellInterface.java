ccbdfd72d28ecc50759d8b57eea1e582cc7c4181
jtbuaa <jtbuaa@gmail.com>
2011-10-20 15:30:06 +0000

    jingtao: add delete system app feature

---
+++ simpleHome/src/simple/home/jtbuaa/ShellInterface.java
@@ -0,0 +1,66 @@
+package simple.home.jtbuaa;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
+
+public class ShellInterface {
+
+	public static String doExec(String[] commands, boolean suNeeded) {
+		Process process = null;
+		DataOutputStream os = null;
+		DataInputStream osRes = null;
+		String res = "";
+
+		try {
+			if (suNeeded) {// Getting Root
+				process = Runtime.getRuntime().exec("su");
+			} else {
+				process = Runtime.getRuntime().exec("sh");
+			}
+
+			os = new DataOutputStream(process.getOutputStream());
+			osRes = new DataInputStream(process.getInputStream());
+			
+			String line = "";
+			// Doing Stuff ;)
+			for (String single : commands) {
+				os.writeBytes(single + "\n");
+				os.flush();
+				//Thread.sleep(200);
+			}
+			
+			os.writeBytes("exit\n");
+			os.flush();
+			while((line = osRes.readLine()) != null) {
+				res += line + "\n";
+				Log.d("============", line);
+			}
+
+			process.waitFor();
+
+        } catch (Exception e) {
+        	res += e.getMessage();
+			Log.d("===============", e.getMessage());
+			e.printStackTrace();
+		} finally {
+			try {
+				if (os != null) {
+					os.close();
+				}
+				if (osRes != null) {
+					osRes.close();
+				}
+				process.destroy();
+			} catch (Exception e) {
+				// nothing
+			}
+		}
+		return res;
+	}
+}

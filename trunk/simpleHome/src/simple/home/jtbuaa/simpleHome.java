7f7aa112723d9ced6fac100a0d89d5c5018f02a7
jtbuaa@gmail.com <jtbuaa@gmail.com@cc3c3b66-2126-11df-b9f8-bfe9128881a6>
2011-11-26 14:51:59 +0000

    jingtao: don't download htm file

--- trunk/simpleHome/src/simple/home/jtbuaa/simpleHome.java
+++ trunk/simpleHome/src/simple/home/jtbuaa/simpleHome.java
@@ -1711,7 +1711,7 @@ public class simpleHome extends Activity implements SensorEventListener, sizedRe
 		String ss[] = url.split("/");
 		String apkName = ss[ss.length-1].toLowerCase() ; //get download file name
 		if (apkName.contains("=")) apkName = apkName.split("=")[apkName.split("=").length-1];
-		if ((apkName.endsWith(".txt")) || (apkName.endsWith(".html"))) return false;//should not download txt and html file.
+		if ((apkName.endsWith(".txt")) || (apkName.endsWith(".html")) || (apkName.endsWith(".htm"))) return false;//should not download txt and html file.
 		
 		MimeTypeMap mimeTypeMap = MimeTypeMap.getSingleton();
 		if (mimeTypeMap.hasExtension(mimeTypeMap.getFileExtensionFromUrl(apkName))) {//files need download

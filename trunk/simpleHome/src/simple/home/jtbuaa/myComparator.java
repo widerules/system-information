f35304a6441061b986d4a5ee98c7f9fbef3efa92
jtbuaa@gmail.com <jtbuaa@gmail.com@cc3c3b66-2126-11df-b9f8-bfe9128881a6>
2011-11-27 13:43:15 +0000

    jingtao: change the sort method which I think will be more efficient

--- trunk/simpleHome/src/simple/home/jtbuaa/myComparator.java
+++ trunk/simpleHome/src/simple/home/jtbuaa/myComparator.java
@@ -8,30 +8,12 @@ import android.content.pm.ResolveInfo;
 import android.util.Log;
 
 public class myComparator implements Comparator<ResolveInfo> {
-	public myComparator(PackageManager pm) {
-	    mPM = pm;
+	public myComparator() {
 	}
 
 	public final int compare(ResolveInfo a, ResolveInfo b) {
-	    CharSequence  sa = a.loadLabel(mPM);
-	    if (sa == null) sa = a.activityInfo.name;
-	    CharSequence  sb = b.loadLabel(mPM);
-	    if (sb == null) sb = b.activityInfo.name;
-
-	    String sa1 = sa.toString().trim();
-	    String sb1 = sb.toString().trim();
-	    
-	    String sa2 = "";
-	    String sb2 = "";
-	    
-	    for (int i = 0; i < sa1.length(); i++)
-	    	sa2 += HanziToPinyin.getInstance().getToken(sa1.charAt(i)).target; 
-	    for (int i = 0; i < sb1.length(); i++)
-	    	sb2 += HanziToPinyin.getInstance().getToken(sb1.charAt(i)).target;
-	    
-	    return sCollator.compare(sa2, sb2);
+	    return sCollator.compare(a.activityInfo.applicationInfo.dataDir, b.activityInfo.applicationInfo.dataDir);
 	}
 
 	private final Collator   sCollator = Collator.getInstance();
-	private PackageManager   mPM;
 }

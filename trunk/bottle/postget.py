5c57435b2d011b516760be026d4d0b3a9bff592f
jtbuaa@gmail.com <jtbuaa@gmail.com@cc3c3b66-2126-11df-b9f8-bfe9128881a6>
2011-12-12 05:41:57 +0000

    jingtao: add a new project: bottle

---
+++ trunk/bottle/postget.py
@@ -0,0 +1,22 @@
+from bottle import get, post, request, run, app
+
+#app = Bottle()
+
+@get('/login') # or @route('/login')
+def login_form():
+    return '''<form method="POST">
+                <input name="name"     type="text" />
+                <input name="password" type="password" />
+                <input name="submit" type="submit" />
+              </form>'''
+
+@post('/login') # or @route('/login', method='POST')
+def login_submit():
+    name     = request.forms.get('name')
+    password = request.forms.get('password')
+    #if check_login(name, password):
+    #    return "<p>Your login was correct</p>"
+    #else:
+    return "<p>Login failed</p>"
+
+run(host='localhost', port=8080)

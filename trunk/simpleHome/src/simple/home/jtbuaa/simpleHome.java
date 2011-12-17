a96fcec1f07324f487e8f0ce2404298a97dbcac0
jtbuaa@gmail.com <jtbuaa@gmail.com@cc3c3b66-2126-11df-b9f8-bfe9128881a6>
2011-12-17 14:39:04 +0000

    jingtao: reset wall paper dimision if set new wallpaper, otherwise it may not show completely

--- trunk/simpleHome/src/simple/home/jtbuaa/simpleHome.java
+++ trunk/simpleHome/src/simple/home/jtbuaa/simpleHome.java
@@ -1627,6 +1627,7 @@ public class simpleHome extends Activity implements SensorEventListener, sizedRe
 				        if (factor >= 1.2) {//if too wide, we want use setWallpaperOffsets to move it, so we need set it to wallpaper
 				        	int tmpWidth = (int) (apps.getHeight() * factor);
 					    	mWallpaperManager.setBitmap(Bitmap.createScaledBitmap(bd.getBitmap(), tmpWidth, apps.getHeight(), true));
+					    	mWallpaperManager.suggestDesiredDimensions(tmpWidth, apps.getHeight());
 			 	 			cbWallPaper.performClick();
 				        }
 				    	else {//otherwise just change the background is ok.				    		

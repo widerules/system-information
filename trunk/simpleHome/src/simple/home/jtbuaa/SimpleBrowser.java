2c0950854f4d180aa684726e934a84c189658ba8
jtbuaa@gmail.com <jtbuaa@gmail.com@cc3c3b66-2126-11df-b9f8-bfe9128881a6>
2012-01-02 01:55:50 +0000

    jingtao: fix issue of open unnecessory new webpage

--- trunk/simpleHome/src/simple/home/jtbuaa/SimpleBrowser.java
+++ trunk/simpleHome/src/simple/home/jtbuaa/SimpleBrowser.java
@@ -721,7 +721,7 @@ BroadcastReceiver downloadReceiver = new BroadcastReceiver() {
 };
 
 private void loadNewPage(String url) {
-	if ((!serverWebs.get(webIndex).title.equals(getString(R.string.app_name))) || serverWebs.get(webIndex).canGoBack()) 
+	if ((!serverWebs.get(webIndex).title.equals(getString(R.string.browser_name))) || serverWebs.get(webIndex).canGoBack()) 
 		btnNewpage.performClick();//open the url in a new page if current page is not the main page
 	serverWebs.get(webIndex).loadUrl(url);
 	serverWebs.get(webIndex).title = url; 

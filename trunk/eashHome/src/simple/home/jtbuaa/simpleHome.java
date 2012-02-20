e5540dfbd1d6532124fde01b29e1156702ca957a
jtbuaa@gmail.com <jtbuaa@gmail.com@cc3c3b66-2126-11df-b9f8-bfe9128881a6>
2012-02-20 02:25:02 +0000

    jingtao: fix issue of can't restart home on G2

--- trunk/eashHome/src/simple/home/jtbuaa/simpleHome.java
+++ trunk/eashHome/src/simple/home/jtbuaa/simpleHome.java
@@ -693,7 +693,7 @@ public class simpleHome extends Activity implements SensorEventListener, sizedRe
 					public void onClick(DialogInterface dialog, int which) {
 						Intent intent = getIntent();
 					    overridePendingTransition(0, 0);
-					    intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+					    intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION | Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
 					    finish();
 					    overridePendingTransition(0, 0);
 					    startActivity(intent);

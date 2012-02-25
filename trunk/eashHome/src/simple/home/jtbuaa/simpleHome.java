cadb8aa98028f77a73177725758f52af19aff5f6
jtbuaa@gmail.com <jtbuaa@gmail.com@cc3c3b66-2126-11df-b9f8-bfe9128881a6>
2012-02-25 10:38:58 +0000

    jingtao: fix issue of can't restart home

--- trunk/eashHome/src/simple/home/jtbuaa/simpleHome.java
+++ trunk/eashHome/src/simple/home/jtbuaa/simpleHome.java
@@ -720,12 +720,10 @@ public class simpleHome extends Activity implements SensorEventListener, sizedRe
 				setPositiveButton(getString(R.string.ok), new DialogInterface.OnClickListener() {
 					@Override
 					public void onClick(DialogInterface dialog, int which) {
-						Intent intent = getIntent();
-					    overridePendingTransition(0, 0);
-					    intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION | Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
 					    finish();
-					    overridePendingTransition(0, 0);
-					    startActivity(intent);
+				        Intent homeIntent = new Intent(Intent.ACTION_MAIN);
+				        homeIntent.addCategory(Intent.CATEGORY_HOME);
+				        startActivity(homeIntent);
 					}
 				}).
 				setNegativeButton(getString(R.string.cancel), new DialogInterface.OnClickListener() {

ed43312c0660b1589a095fc4a764be0d895a2e0d
jtbuaa <jtbuaa@gmail.com>
2012-02-25 08:00:17 +0000

    jingtao: fix issue of save null title history

--- EasyLib/src/easy/lib/util.java
+++ EasyLib/src/easy/lib/util.java
@@ -37,7 +37,8 @@ class TitleUrl {
 	String m_site;
 	
 	TitleUrl(String title, String url, String site) {
-		m_title =  title;
+		if (title != null) m_title = title;
+		else m_title = url;
 		m_url = url;
 		m_site = site;
 	}

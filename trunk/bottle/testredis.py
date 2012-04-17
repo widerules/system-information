539b1345e9b5ce83758adddcede742d06ba42be5
jtbuaa@gmail.com <jtbuaa@gmail.com@cc3c3b66-2126-11df-b9f8-bfe9128881a6>
2012-04-17 07:57:35 +0000

    jingtao: remove bottle project

--- trunk/bottle/testredis.py
+++
@@ -1,6 +0,0 @@
-import redis
-r = redis.StrictRedis(host='localhost', port=6379, db=0)
-r.set('foo','bar')
-r.set('foo1','bar1')
-print 'dbsize:%s' %r.dbsize()
-print 'key value is:%s' %r.get('fool')

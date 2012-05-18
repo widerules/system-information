e80ad4fbcad872ac2719ab0e231b11b784e14c98
jtbuaa <jtbuaa@gmail.com>
2012-05-18 08:48:27 +0000

    jingtao: sort the bookmark

--- EasyLib/src/easy/lib/SimpleBrowser.java
+++ EasyLib/src/easy/lib/SimpleBrowser.java
@@ -14,6 +14,7 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLDecoder;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
@@ -1970,6 +1971,7 @@ private void addFavo(final String url, final String title) {
 					if ("".equals(title)) title += (char)0xa0;//add a blank character to occupy the space
 					TitleUrl titleUrl = new TitleUrl(title, url, site);
 		    		mBookMark.add(titleUrl);
+		    		Collections.sort(mBookMark, new myComparator());//sort by name
 		    		loadPage(false);
 		    		
 		    		bookmarkChanged = true;

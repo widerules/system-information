37b81912afb820e58b5db7a588d13a9f435db369
jtbuaa@gmail.com <jtbuaa@gmail.com@cc3c3b66-2126-11df-b9f8-bfe9128881a6>
2012-09-08 15:05:44 +0000

    jingtao: fix the null pointer issue when compare

--- trunk/EasyLib/src/easy/lib/stringComparator.java
+++ trunk/EasyLib/src/easy/lib/stringComparator.java
@@ -10,7 +10,9 @@ public class stringComparator implements Comparator<String> {
 	}
 
 	public final int compare(String a, String b) {
-		return sCollator.compare(a, b);
+		if (a == null) return -1;
+		else if (b == null) return 1;
+		else return sCollator.compare(a, b);
 	}
 
 	private final Collator sCollator = Collator.getInstance();

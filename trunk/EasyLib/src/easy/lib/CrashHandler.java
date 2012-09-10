b6de88599478906a3703bcc1123fef41a53ebc94
jtbuaa@gmail.com <jtbuaa@gmail.com@cc3c3b66-2126-11df-b9f8-bfe9128881a6>
2012-09-10 07:57:53 +0000

    jingtao: add crash control

--- trunk/EasyLib/src/easy/lib/CrashHandler.java
+++ trunk/EasyLib/src/easy/lib/CrashHandler.java
@@ -13,6 +13,9 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
@@ -21,56 +24,40 @@ import android.os.Looper;
 import android.util.Log;
 import android.widget.Toast;
 
-/**
- * UncaughtException处理类,当程序发生Uncaught异常的时候,有该类来接管程序,并记录发送错误报告.
- * 
- * @author user
- * 
- */
 public class CrashHandler implements UncaughtExceptionHandler {
 
-	// 系统默认的UncaughtException处理类
+	// system default UncaughtException handler
 	private Thread.UncaughtExceptionHandler mDefaultHandler;
-	// CrashHandler实例
 	private static CrashHandler INSTANCE = new CrashHandler();
-	// 程序的Context对象
 	private Context mContext;
-	// 用来存储设备信息和异常信息
-	private Map<String, String> infos = new HashMap<String, String>();
 
-	// 用于格式化日期,作为日志文件名的一部分
-	private DateFormat formatter = new SimpleDateFormat("yyyy-MM-dd-HH-mm-ss");
-
-	/** 保证只有一个CrashHandler实例 */
+	// notification id
+	int ID = 22994904;
+	
+	// to ensure only have one CrashHandler
 	private CrashHandler() {
 	}
 
-	/** 获取CrashHandler实例 ,单例模式 */
+	// get instance of CrashHandler, singleton mode
 	public static CrashHandler getInstance() {
 		return INSTANCE;
 	}
 
-	/**
-	 * 初始化
-	 * 
-	 * @param context
-	 */
 	public void init(Context context) {
 		mContext = context;
 
-		// 获取系统默认的UncaughtException处理器
+		// get system default UncaughtException handler and set it as handler of this app
 		mDefaultHandler = Thread.getDefaultUncaughtExceptionHandler();
-		// 设置该CrashHandler为程序的默认处理器
 		Thread.setDefaultUncaughtExceptionHandler(this);
 	}
 
 	/**
-	 * 当UncaughtException发生时会转入该函数来处理
+	 * use this to handle UncaughtException
 	 */
 	@Override
 	public void uncaughtException(Thread thread, Throwable ex) {
 		if (!handleException(ex) && mDefaultHandler != null) {
-			// 如果用户没有处理则让系统默认的异常处理器来处理
+			// handle by system default exception handler if no user defined
 			mDefaultHandler.uncaughtException(thread, ex);
 		} else {
 			try {
@@ -78,33 +65,17 @@ public class CrashHandler implements UncaughtExceptionHandler {
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
-			// 退出程序
+			// exit
 			android.os.Process.killProcess(android.os.Process.myPid());
 			System.exit(1);
 		}
 	}
 
-	/**
-	 * 自定义错误处理,收集错误信息 发送错误报告等操作均在此完成.
-	 * 
-	 * @param ex
-	 * @return true:如果处理了该异常信息;否则返回false.
-	 */
 	private boolean handleException(Throwable ex) {
 		if (ex == null) {
 			return false;
 		}
 		ex.printStackTrace();
-
-		// collect device info
-		collectDeviceInfo(mContext);
-
-		StringBuffer sb = new StringBuffer();
-		for (Map.Entry<String, String> entry : infos.entrySet()) {
-			String key = entry.getKey();
-			String value = entry.getValue();
-			sb.append(key + "=" + value + "\n");
-		}
 
 		Writer writer = new StringWriter();
 		PrintWriter printWriter = new PrintWriter(writer);
@@ -115,81 +86,31 @@ public class CrashHandler implements UncaughtExceptionHandler {
 			cause = cause.getCause();
 		}
 		printWriter.close();
-		String result = writer.toString();
-		sb.append(result);
 
-		Intent intent = new Intent(Intent.ACTION_SENDTO);
-		intent.setData(Uri.fromParts("mailto",
-				mContext.getString(R.string.browser_author), null));
-		intent.putExtra(Intent.EXTRA_TEXT, mContext.getString(R.string.feedback) + "\n\n\n\n\n====================\n" + sb.toString());
-		intent.putExtra(Intent.EXTRA_SUBJECT, mContext.getPackageName()
-				+ mContext.getString(R.string.sorry));
-		if (!util.startActivity(intent, false, mContext)) {
-			// save the error log
-			final String path = saveCrashInfo2File(sb);
-			// show toast
-			new Thread() {
-				@Override
-				public void run() {
-					Looper.prepare();
-					Toast.makeText(
-							mContext,
-							mContext.getPackageName()
-									+ mContext.getString(R.string.sorry) + path,
-							Toast.LENGTH_LONG).show();
-					Looper.loop();
-				}
-			}.start();
-		}
+		
+		Intent intent = new Intent();
+		intent.setAction(mContext.getPackageName() + ".crashControl");
+		
+		intent.putExtra("errorMsg", writer.toString());
+		intent.putExtra("id", ID);
+		// request_code will help to diff different thread
+		PendingIntent contentIntent = PendingIntent.getActivity(mContext,
+				ID, intent,
+				PendingIntent.FLAG_UPDATE_CURRENT);
+		
+		Notification notification = new Notification(
+				android.R.drawable.stat_notify_error,
+				"error",
+				System.currentTimeMillis());
+
+		notification.setLatestEventInfo(mContext, 
+				mContext.getString(R.string.browser_name) + mContext.getString(R.string.crashed),
+				mContext.getString(R.string.click_for_detail), contentIntent);
+		
+		NotificationManager nManager = (NotificationManager) mContext.getSystemService(android.content.Context.NOTIFICATION_SERVICE);
+		nManager.notify(ID, notification);
 
 		return true;
 	}
 
-	/**
-	 * 收集设备参数信息
-	 * 
-	 * @param ctx
-	 */
-	public void collectDeviceInfo(Context ctx) {
-		Field[] fields = Build.class.getDeclaredFields();
-		for (Field field : fields) {
-			try {
-				field.setAccessible(true);
-				infos.put(field.getName(), field.get(null).toString());
-				Log.d(field.getName(), field.get(null).toString());
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
-
-		infos.put("versionName", util.getVersion(ctx));
-		infos.put("versionCode", util.getVersionCode(ctx));
-	}
-
-	/**
-	 * 保存错误信息到文件中
-	 * 
-	 * @param ex
-	 * @return 返回文件名称,便于将文件传送到服务器
-	 */
-	private String saveCrashInfo2File(StringBuffer sb) {
-
-		String path = util.preparePath(mContext) + "crash/";
-		try {
-			long timestamp = System.currentTimeMillis();
-			String time = formatter.format(new Date());
-			String fileName = "crash-" + time + "-" + timestamp + ".log";
-			File dir = new File(path);
-			if (!dir.exists()) {
-				dir.mkdirs();
-			}
-			FileOutputStream fos = new FileOutputStream(path + fileName);
-			fos.write(sb.toString().getBytes());
-			fos.close();
-			return path + fileName;
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return "";
-	}
 }

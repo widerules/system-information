c041e89d250301187b9bbed4987186f81549fa0c
jtbuaa@gmail.com <jtbuaa@gmail.com@cc3c3b66-2126-11df-b9f8-bfe9128881a6>
2013-04-09 07:04:13 +0000

    jingtao: add insertion sort

--- trunk/practice/test.c
+++ trunk/practice/test.c
@@ -1,3 +1,19 @@
+void insert_sort(char* str, int len)
+{
+	int i;
+	for (i = 1; i < len; i++)
+	{
+		int j = i - 1;
+		char key = str[i];
+		while (j >= 0 && (str[j] > key))
+		{
+			str[j+1] = str[j];
+			j--;
+		}
+		str[j+1] = key;
+	}
+}
+
 void revert(char* str, int len)
 {
 	if (len > 1)
@@ -47,5 +63,10 @@ void main()
 
 	strcpy(s3, s1);
 	block_revert(s3);
+	printf("s3 is %s\n", s3);
+
+	memset(s3, 0, 128);
+	strcpy(s3, "54321edcba");
+	insert_sort(s3, strlen(s3));
 	printf("s3 is %s\n", s3);
 }

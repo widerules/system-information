ed74f5e62c1e66f820fa4d261f41b9f4772f0dc2
jtbuaa@gmail.com <jtbuaa@gmail.com@cc3c3b66-2126-11df-b9f8-bfe9128881a6>
2013-04-15 11:58:42 +0000

    jingtao: fix potential issue of calculate menu height

--- trunk/HarleyLib/src/easy/lib/SimpleBrowser.java
+++ trunk/HarleyLib/src/easy/lib/SimpleBrowser.java
@@ -3502,8 +3502,8 @@ public class SimpleBrowser extends Activity {
         if (bookmarkWidth > minWidth) bookmarkWidth = minWidth;
         
 		int height = (int) (dm.heightPixels / dm.density);
-		if (showUrl) height -= urlHeight;
-		if (showControlBar) height -= barHeight;
+		height -= urlLine.getHeight()*dm.density;
+		height -= webTools.getHeight()*dm.density;
 		int size = height / 72;// 72 is the height of each menu item
 		if (size > 9) {
 			menuWidth = (int) (80*dm.density);// 80 dip for single column

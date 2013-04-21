7ca88dc4f6bbce73847b43b8a44c0a4897bcb96e
jtbuaa <jtbuaa@gmail.com>
2013-04-21 00:22:46 +0000

    jingtao: more smart set default

--- EasyLib/src/easy/lib/SimpleBrowser.java
+++ EasyLib/src/easy/lib/SimpleBrowser.java
@@ -1883,39 +1883,47 @@ public class SimpleBrowser extends Activity {
 		List<ResolveInfo> resolveInfoList = pm.queryIntentActivities(intent, PackageManager.GET_INTENT_FILTERS);
 		int size = resolveInfoList.size();
 		ComponentName[] arrayOfComponentName = new ComponentName[size];
+		boolean seted = false;
 		for (int i = 0; i < size; i++) {
 			ActivityInfo activityInfo = resolveInfoList.get(i).activityInfo;
 			String packageName = activityInfo.packageName;
 			String className = activityInfo.name;
 			//clear default browser
+			if (packageName.equals(mContext.getPackageName())) {
+				seted = true;
+				break;
+			}
 			try{pm.clearPackagePreferredActivities(packageName);} catch(Exception e) {}
 			ComponentName componentName = new ComponentName(packageName, className);
 			arrayOfComponentName[i] = componentName;
 		}
 		
-		ComponentName component = new ComponentName(mContext.getPackageName(), "easy.lib.SimpleBrowser");
-		pm.addPreferredActivity(filter,	IntentFilter.MATCH_CATEGORY_SCHEME, arrayOfComponentName, component);
+		if (!seted) {
+			ComponentName component = new ComponentName(mContext.getPackageName(), "easy.lib.SimpleBrowser");
+			pm.addPreferredActivity(filter,	IntentFilter.MATCH_CATEGORY_SCHEME, arrayOfComponentName, component);
+		}
 	}
 
 	public void setAsDefaultApp() {
 		PackageManager pm = getPackageManager();
 		
-		try {pm.addPackageToPreferred(getPackageName());} catch(Exception e) {} // for 1.5 platform
-		
-		// set default browser for 1.6-2.1 platform. not work for 2.2 and up platform
-		Intent intent = new Intent("android.intent.action.VIEW");
-		intent.addCategory("android.intent.category.BROWSABLE");
-		intent.addCategory("android.intent.category.DEFAULT");
-		
-		IntentFilter filter = new IntentFilter();
-		filter.addAction("android.intent.action.VIEW");
-		filter.addCategory("android.intent.category.BROWSABLE");
-		filter.addCategory("android.intent.category.DEFAULT");
-		filter.addDataScheme("http");
-		
-		Uri uri = Uri.parse("http://");
-		intent.setDataAndType(uri, null);
-		setDefault(pm, intent, filter);		
+		try {pm.addPackageToPreferred(getPackageName());}// for 1.5 platform 
+		catch(Exception e) {
+			// set default browser for 1.6-2.1 platform. not work for 2.2 and up platform
+			Intent intent = new Intent("android.intent.action.VIEW");
+			intent.addCategory("android.intent.category.BROWSABLE");
+			intent.addCategory("android.intent.category.DEFAULT");
+			
+			IntentFilter filter = new IntentFilter();
+			filter.addAction("android.intent.action.VIEW");
+			filter.addCategory("android.intent.category.BROWSABLE");
+			filter.addCategory("android.intent.category.DEFAULT");
+			filter.addDataScheme("http");
+			
+			Uri uri = Uri.parse("http://");
+			intent.setDataAndType(uri, null);
+			setDefault(pm, intent, filter);			
+		} 
 	}
 	
 	public void readPreference() {

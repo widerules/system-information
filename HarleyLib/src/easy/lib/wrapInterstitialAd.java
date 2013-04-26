cb329e6720de9549f36c09fe0305a043856c6f77
jtbuaa <jtbuaa@gmail.com>
2013-04-26 06:05:39 +0000

    jingtao: change proguard.cfg for interstitial ad

---
+++ HarleyLib/src/easy/lib/wrapInterstitialAd.java
@@ -0,0 +1,77 @@
+package easy.lib;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+
+import com.google.ads.Ad;
+import com.google.ads.AdListener;
+import com.google.ads.AdRequest;
+import com.google.ads.InterstitialAd;
+import com.google.ads.AdRequest.ErrorCode;
+
+import easy.lib.wrapAdView.Listener;
+
+public class wrapInterstitialAd {
+	InterstitialAd mInstance;
+	AdRequest adRequest;
+	Handler mHandler;
+
+	public wrapInterstitialAd(Activity activity, String publisherID, Handler handler) {
+		mInstance = new InterstitialAd(activity, publisherID);
+		mInstance.setAdListener(new Listener());
+		adRequest = new AdRequest();
+		mHandler = handler;
+	}
+
+	boolean isReady() {
+		if (mInstance != null)
+			return mInstance.isReady();
+		else return false;
+	}
+	
+	void show() {
+		if (mInstance != null) mInstance.show();
+	}
+	
+	void loadAd() {
+		if ((mInstance != null) && (adRequest != null))
+			mInstance.loadAd(adRequest);
+	}
+	
+	class Listener implements AdListener {
+		@Override
+		public void onDismissScreen(Ad arg0) {
+		}
+
+		@Override
+		public void onFailedToReceiveAd(Ad arg0, ErrorCode error) {
+			if (mHandler != null) {
+				Message fail = mHandler.obtainMessage();
+				Bundle data = new Bundle();
+				data.putString("msg", "onFailedToReceiveAd (" + error + ")");
+				fail.setData(data);
+				fail.what = -2;
+				mHandler.sendMessage(fail);
+			}
+		}
+
+		@Override
+		public void onLeaveApplication(Ad arg0) {
+			if (mHandler != null) {
+				Message dismiss = mHandler.obtainMessage();
+				dismiss.what = 1;
+				mHandler.sendMessage(dismiss);
+			}
+		}
+
+		@Override
+		public void onPresentScreen(Ad arg0) {
+		}
+
+		@Override
+		public void onReceiveAd(Ad arg0) {
+		}
+	}
+}

7fad7e79ae4db7a74a76fff1dcc6b7fdad56411b
jtbuaa@gmail.com <jtbuaa@gmail.com@cc3c3b66-2126-11df-b9f8-bfe9128881a6>
2013-06-20 01:45:10 +0000

    jingtao: add baselib for home and browser

---
+++ trunk/BaseLib/src/base/lib/MyViewFlipper.java
@@ -0,0 +1,24 @@
+package base.lib;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ViewFlipper;
+
+public class MyViewFlipper extends ViewFlipper {
+	public MyViewFlipper(Context context) {
+		super(context);
+	}
+
+	public MyViewFlipper(Context context, AttributeSet attrs) {
+		super(context, attrs);
+	}
+
+	@Override
+	protected void onDetachedFromWindow() {
+		try {
+			super.onDetachedFromWindow();
+		} catch (IllegalArgumentException e) {
+			stopFlipping();
+		}
+	}
+}

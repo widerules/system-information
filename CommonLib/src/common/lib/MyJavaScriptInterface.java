ec5ead4949f597e1fbf06ff862742f8f768073af
jtbuaa <jtbuaa@gmail.com>
2013-07-03 09:07:38 +0000

    jingtao: fix build error

---
+++ CommonLib/src/common/lib/MyJavaScriptInterface.java
@@ -0,0 +1,29 @@
+package common.lib;
+
+import android.os.Message;
+import android.widget.Toast;
+
+public class MyJavaScriptInterface {
+	MyWebView mWebView;
+	MyApp mAppstate;
+	MyJavaScriptInterface(MyWebView webview, MyApp appstate) {
+		mWebView = webview;
+		mAppstate = appstate;
+	}
+	
+	@SuppressWarnings("unused")
+	public void processHTML(String html) {
+		mWebView.pageSource = html;// to get page source, part 1
+	}
+
+	@SuppressWarnings("unused")
+	public void showInterstitialAd() {
+		if (mAppstate.interstitialAd.isReady()) mAppstate.interstitialAd.show();
+		else {
+			Toast.makeText(mAppstate.mContext, "Admob is loading", Toast.LENGTH_LONG).show();
+			Message fail = mAppstate.mAppHandler.obtainMessage();
+			fail.what = -3;
+			mAppstate.mAppHandler.sendMessage(fail);
+		}
+	}
+}
--- CommonLib/src/common/lib/MyWebView.java
+++ CommonLib/src/common/lib/MyWebView.java
@@ -90,67 +90,6 @@ class MyWebView extends WebView {
 		loadUrl("javascript:window.JSinterface.processReady(document.getElementById('title1').innerHTML);");
 	}
 
-	class MyJavaScriptInterface {
-		@SuppressWarnings("unused")
-		public void processHTML(String html) {
-			pageSource = html;// to get page source, part 1
-		}
-		
-		@SuppressWarnings("unused")
-		public void processReady(String ready) {
-			m_ready = ready;
-		}
-		
-		@SuppressWarnings("unused")
-		public void saveCollapseState(String item, boolean state) {
-			if ("1".equals(item))
-				collapse1 = state;
-			if ("2".equals(item))
-				collapse2 = state;
-			if ("3".equals(item))
-				collapse3 = state;
-		}
-		
-		@SuppressWarnings("unused")
-		public void deleteItems(String bookmarks, String historys) {
-			if (!"".equals(historys) && !",,,,".equals(historys)) {
-				String[] tmp1 = historys.split(",,,,");
-				for (int i = 0; i < tmp1.length; i++) {// the display of history is in revert order, so delete in revert order
-					int index = mAppstate.mHistory.size() - 1 + i - (Integer.valueOf(tmp1[i]) - mAppstate.mBookMark.size());
-					if ((index >= 0) && (index < mAppstate.mHistory.size()))// sometime the index is -1? 
-						mAppstate.mHistory.remove(index);
-				}
-				mAppstate.updateHistory();
-				mAppstate.historyChanged = true;
-			}
-			
-			if (!"".equals(bookmarks) && !",,,,".equals(bookmarks)) {
-				String[] tmp2 = bookmarks.split(",,,,");
-				for (int i = tmp2.length-1; i >= 0; i--) 
-					try{mAppstate.mBookMark.remove(Integer.valueOf(tmp2[i]) + 0);} catch(Exception e) {}// it will not treat as integer if not add 0
-				mAppstate.updateBookmark();
-				mAppstate.bookmarkChanged = true;
-			}
-		}
-		
-		@SuppressWarnings("unused")
-		public void updateHome() {
-			mAppstate.updateHomePage();
-		}
-		
-		@SuppressWarnings("unused")
-		public void showInterstitialAd() {
-			if (mAppstate.interstitialAd.isReady()) mAppstate.interstitialAd.show();
-			else {
-				Toast.makeText(mContext, "Admob is loading", Toast.LENGTH_LONG).show();
-				Message fail = mAppstate.mAppHandler.obtainMessage();
-				fail.what = -3;
-				mAppstate.mAppHandler.sendMessage(fail);
-			}
-		}
-	}
-
-	@SuppressLint({ "NewApi", "NewApi" })
 	public void setZoomControl(int visibility) {
 		Class<?> classType;
 		Field field;
@@ -251,7 +190,7 @@ class MyWebView extends WebView {
 		//registerForContextMenu(this);// =========================================================compile error here
 
 		// to get page source, part 2
-		addJavascriptInterface(new MyJavaScriptInterface(), "JSinterface");
+		addJavascriptInterface(new MyJavaScriptInterface(this, mAppstate), "JSinterface");
 
 		setDownloadListener(new DownloadListener() {
 			@Override

6d6e66349386fbc37b5c4fc2dfd707ef11621e23
jtbuaa <jtbuaa@gmail.com>
2013-07-08 02:27:47 +0000

    jingtao: send message when receive ad

--- BaseLib/src/base/lib/WrapAdView.java
+++ BaseLib/src/base/lib/WrapAdView.java
@@ -70,6 +70,11 @@ public class WrapAdView {
 
 		@Override
 		public void onReceiveAd(Ad arg0) {
+			if (mHandler != null) {
+				Message dismiss = mHandler.obtainMessage();
+				dismiss.what = 0;
+				mHandler.sendMessage(dismiss);
+			}
 		}
 	}
 
